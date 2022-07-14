b24f8238660d99304f6d89f036a4903dbc02aa3c
shreegowri <gaganandh@gmail.com>
2022-07-14 22:26:20 +0530

    [shreegowri]creating the Employee pay roll table

--- EmployeePayRollService/UC1_CreatTable.sql
+++ EmployeePayRollService/UC1_CreatTable.sql
@@ -4,3 +4,13 @@ create database Payroll_Service;
 -----drop database AddressBookDataBase;-------
 -------to get into the db-----
 use Payroll_Service;
+
+
+----to create a table UC2---
+create Table employee_payroll(
+      Id int identity(1,1) PRIMARY KEY,--first 1=it should be start with  first  row and second indicate 1=increment by 1---
+	  Name varchar(200),
+	  Salary float,
+	  StartDate datetime 
+
+);
