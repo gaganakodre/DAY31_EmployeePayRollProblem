37b111799bf78ead27dc89d78bb97555c1749567
shreegowri <gaganandh@gmail.com>
2022-07-14 22:22:28 +0530

    [shreegowri]createing the employeepay roll service data base

---
+++ EmployeePayRollService/UC1_CreatTable.sql
@@ -0,0 +1,6 @@
+-------create database--------
+create database Payroll_Service;
+------delete the database--------
+-----drop database AddressBookDataBase;-------
+-------to get into the db-----
+use Payroll_Service;
