5e489367eb98740671c31f5f15a7f1bb58c3d3ac
shreegowri <gaganandh@gmail.com>
2022-07-15 11:43:37 +0530

    [shreegowri]adding to the main

---
+++ EmployeePayRollService/dbo.employee_payroll.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[employee_payroll] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      VARCHAR (200) NULL,
+    [Salary]    FLOAT (53)    NULL,
+    [StartDate] DATETIME      NULL,
+    [Gender]    CHAR (1)      NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
